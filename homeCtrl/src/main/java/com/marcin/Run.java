c88e2f9cf645a15c696eefe61e26cb3f1d5c7faf
mbogusiak <mbogusiak@gmail.com>
2014-04-02 10:05:21 +0200

    com2

--- homeCtrl/src/main/java/com/marcin/Run.java
+++ homeCtrl/src/main/java/com/marcin/Run.java
@@ -7,7 +7,7 @@ public class Run {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("HelloWorld");
+		System.out.println("HelloWorldTest");
 	}
 
 }
