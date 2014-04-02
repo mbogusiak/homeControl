08a751f50d7cb9dd3cffea53eb1ea6c0a10bdf95
mbogusiak <mbogusiak@gmail.com>
2014-04-02 10:03:52 +0200

    com1

---
+++ homeCtrl/src/main/java/com/marcin/Run.java
@@ -0,0 +1,13 @@
+package com.marcin;
+
+public class Run {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("HelloWorld");
+	}
+
+}
